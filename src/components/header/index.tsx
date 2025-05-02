b84cc69143f8f536a586e5d794884beb861bdf59
Renan Borges <renanvcb@gmail.com>
2025-05-01 22:02:50 -0300

    feat: adjust header style to fit responsive

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -13,7 +13,7 @@ export function Header() {
   };
 
   return (
-    <header className="flex items-center justify-between p-4 bg-white">
+    <header className="flex items-center justify-between p-4 bg-white md:py-6 md:px-20">
       <div className="flex items-center">
         <Link href="/">
           <Image

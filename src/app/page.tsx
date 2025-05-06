ca8f522cabe4793cc9db36c1df17c42978e007a8
Renan Borges <renanvcb@gmail.com>
2025-05-06 20:20:51 -0300

    fix: add Features component to Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Features } from "@/components/features";
 import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <div className="py-12 px-4 lg:p-20">
+    <div className="py-12 px-4 lg:p-20 space-y-24">
       <Hero />
+      <Features />
     </div>
   );
 }

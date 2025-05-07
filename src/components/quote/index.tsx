dd4088b2955dc27adce0c9e036f837d6481aa3a0
Renan Borges <renanvcb@gmail.com>
2025-05-07 11:25:26 -0300

    feat: enhance Quote component with additional content and structure

--- src/components/quote/index.tsx
+++ src/components/quote/index.tsx
@@ -8,9 +8,36 @@ export function Quote() {
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.6 }}
       viewport={{ once: true, amount: 0.5 }}
-      className="bg-yellow-400"
+      className="bg-yellow-400 py-12"
     >
-      <h2>Quote section</h2>
+      <motion.div
+        className="space-y-12"
+        initial={{ opacity: 0, x: -30 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.6 }}
+        viewport={{ once: true, amount: 0.5 }}
+      >
+        <div className="px-4 text-lg space-y-4">
+          <p className="leading-loose">
+            Id urna, nisl, ut quam. Diam suspendisse fringilla quam arcu mattis
+            est velit in. Nibh in purus sit convallis phasellus ut. At vel erat
+            ultricies commodo. Neque suspendisse a habitasse commodo.
+          </p>
+
+          <p className="leading-tight">
+            Marie Poirot,
+            <br />
+            <span className="font-medium">Bigapp</span>
+          </p>
+        </div>
+
+        <div className="px-4 flex gap-2">
+          <div className="w-8 h-2 bg-slate-950 rounded-full" />
+          <div className="size-2 bg-white rounded-full" />
+          <div className="size-2 bg-white rounded-full" />
+          <div className="size-2 bg-white rounded-full" />
+        </div>
+      </motion.div>
     </motion.section>
   );
 }

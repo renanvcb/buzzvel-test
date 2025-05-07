f81f1e83c827409fe1ebaebf941316833c72bad4
Renan Borges <renanvcb@gmail.com>
2025-05-06 21:28:39 -0300

    feat: enhance Navbar component with additional links and improved layout

--- src/components/header/navbar/index.tsx
+++ src/components/header/navbar/index.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/button";
 import Link from "next/link";
 import { IoMenuOutline } from "react-icons/io5";
+import { FaChevronDown } from "react-icons/fa6";
 
 interface NavbarProps {
   toggleSidebar: () => void;
@@ -7,12 +9,30 @@ interface NavbarProps {
 
 export function Navbar({ toggleSidebar }: NavbarProps) {
   return (
-    <nav>
-      <div className="hidden lg:flex lg:space-x-4">
-        <Link href="#">Home</Link>
-        <Link href="#">About</Link>
-        <Link href="#">Services</Link>
-        <Link href="#">Contact</Link>
+    <nav className="flex items-center">
+      <div className="hidden items-center gap-36 lg:flex xl:gap-[512px]">
+        <div className="gap-8 flex xl:gap-12 font-medim">
+          <Link href="#">Products</Link>
+          <Link href="#">Solutions</Link>
+          <Link href="#">Pricing</Link>
+          <Link href="#" className="flex items-center gap-2">
+            Resources
+            <FaChevronDown />
+          </Link>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <a href="/" className="font-medium text-nowrap">
+            Log In
+          </a>
+          <Button
+            variant="outline"
+            className="!border-black !text-black"
+            size="m"
+          >
+            <Button.Text>Sign Up Now</Button.Text>
+          </Button>
+        </div>
       </div>
 
       <button type="button" className="lg:hidden" onClick={toggleSidebar}>

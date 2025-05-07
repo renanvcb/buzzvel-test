77f77b384d850c3fdc98a59d54c3c91456cacf8b
Renan Borges <renanvcb@gmail.com>
2025-05-06 23:43:14 -0300

    feat: enhance Features component with new grid layout for lessons

--- src/components/features/index.tsx
+++ src/components/features/index.tsx
@@ -61,7 +61,8 @@ export function Features() {
         viewport={{ once: true, amount: 0.4 }}
         className="w-full"
       >
-        <div className="border-8 border-white bg-white rounded-xl shadow-lg shadow-black">
+        <div className="relative mx-4 border-8 border-white bg-white rounded-xl shadow-xs shadow-black">
+          {/* Iframe container */}
           <div className="py-2.5 flex gap-1.5">
             <div className="size-2.5 bg-rose-600 rounded-full" />
             <div className="size-2.5 bg-amber-400 rounded-full" />
@@ -78,6 +79,62 @@ export function Features() {
               allowFullScreen
               className="w-full h-full"
             />
+          </div>
+
+          {/* Grid container positioned on top */}
+          <div className="absolute top-[195px] -right-10 grid grid-cols-2 gap-4 p-4 md:grid-cols-3 bg-white shadow-lg rounded-lg">
+            <div className="shadow-md/30 shadow-black p-4 space-y-2">
+              <div className="bg-purple-100 text-purple-800 px-2 py-1 rounded-sm inline-block">
+                Featured
+              </div>
+              <h3 className="font-medium">The map of mathematics</h3>
+
+              <p className="text-xs text-slate-600">
+                Egestas elit dui scelerisque ut eu purus aliquam vitae
+                habitasse.
+              </p>
+
+              <div className="flex justify-center mt-4">
+                <Button variant="outline" size="s">
+                  <Button.Text>Take Lesson</Button.Text>
+                </Button>
+              </div>
+            </div>
+
+            <div className="shadow-md/30 shadow-black p-4 space-y-2">
+              <div className="bg-blue-100 text-blue-800 px-2 py-1 rounded-sm inline-block">
+                Popular
+              </div>
+              <h3 className="font-medium">Design for how people think</h3>
+
+              <p className="text-xs text-slate-600">
+                Aliquam ut euismod condimentum elementum ultricies volutpat sit
+                non.
+              </p>
+
+              <div className="flex justify-center mt-4">
+                <Button variant="outline" size="s">
+                  <Button.Text>Take Lesson</Button.Text>
+                </Button>
+              </div>
+            </div>
+
+            <div className="hidden md:block shadow-md/30 shadow-black p-4 space-y-2">
+              <div className="bg-green-100 text-green-800 px-2 py-1 rounded-sm inline-block">
+                New
+              </div>
+              <h3 className="font-medium">International & commercial law</h3>
+
+              <p className="text-xs text-slate-600">
+                Molestie integer eu arcu, mauris bibendum rhoncus imperdiet dui.
+              </p>
+
+              <div className="flex justify-center mt-4">
+                <Button variant="outline" size="s">
+                  <Button.Text>Take Lesson</Button.Text>
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
       </motion.div>

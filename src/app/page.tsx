3885891be048fb8e09b787af7385ffe275062056
Renan Borges <renanvcb@gmail.com>
2025-05-07 20:56:51 -0300

    feat: reduced space between sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 import { Features } from "@/components/features";
 import { Hero } from "@/components/hero";
+import { JoinUs } from "@/components/join-us";
 import { Quote } from "@/components/quote";
 import { Stats } from "@/components/stats";
 import { Students } from "@/components/students";
 
 export default function Home() {
   return (
-    <div className="py-12 space-y-20">
+    <div className="py-12 space-y-14">
       <Hero />
       <Features />
       <Quote />
       <Students />
       <Stats />
+      <JoinUs />
     </div>
   );
 }

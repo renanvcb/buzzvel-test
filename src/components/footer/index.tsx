b6a5951ced6e98e1a64f4e8212a5bdc85740e62e
Renan Borges <renanvcb@gmail.com>
2025-05-07 23:44:36 -0300

    feat: update footer component with new logo and improved layout

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -12,57 +12,66 @@ export function Footer() {
       whileInView={{ opacity: 1 }}
       transition={{ duration: 0.6 }}
       viewport={{ once: true, amount: 0.5 }}
-      className="-mt-14 px-4 py-12 bg-slate-900 text-white space-y-10"
+      className="-mt-14 px-4 py-12 bg-slate-900 text-white md:px-20"
     >
-      <Image src="/logo-white.svg" alt="Uteach logo" width={361} height={35} />
+      <div className="space-y-10 border-b border-slate-700 pb-12 md:grid md:grid-cols-3 xl:grid-cols-6">
+        <Image
+          src="/logo-white.png"
+          alt="Uteach logo"
+          width={174}
+          height={36}
+        />
 
-      <div className="space-y-6 py-3">
-        <h3 className="font-medium">Product</h3>
-        <p>Pricing</p>
-        <p>Overview</p>
-        <p>Browse</p>
-        <div className="flex gap-2">
-          <p>Accessibility</p>
-          <p className="px-2 rounded-md bg-purple-100 text-purple-800">BETA</p>
+        <div className="space-y-6 py-3">
+          <h3 className="font-medium">Product</h3>
+          <p>Pricing</p>
+          <p>Overview</p>
+          <p>Browse</p>
+          <div className="flex gap-2">
+            <p>Accessibility</p>
+            <p className="px-2 rounded-md bg-purple-100 text-purple-800">
+              BETA
+            </p>
+          </div>
+        </div>
+
+        <div className="space-y-6">
+          <h3 className="font-medium">Solutions</h3>
+          <p>Brainstorming</p>
+          <p>Ideation</p>
+          <p>Wireframing</p>
+          <p>Research</p>
+        </div>
+
+        <div className="space-y-6">
+          <h3 className="font-medium">Resources</h3>
+          <p>Help Center</p>
+          <p>Blog</p>
+          <p>Tutorials</p>
+          <p>FAQs</p>
+        </div>
+
+        <div className="space-y-6">
+          <h3 className="font-medium">Support</h3>
+          <p>Contact Us</p>
+          <p>Developers</p>
+          <p>Documentation</p>
+          <p>Integrations</p>
+        </div>
+
+        <div className="space-y-6">
+          <h3 className="font-medium">Company</h3>
+          <p>About</p>
+          <p>Press</p>
+          <p>Events</p>
+          <div className="flex items-center gap-2">
+            <p>Request Demo</p>
+            <FaArrowRight />
+          </div>
         </div>
       </div>
 
-      <div className="space-y-6">
-        <h3 className="font-medium">Solutions</h3>
-        <p>Brainstorming</p>
-        <p>Ideation</p>
-        <p>Wireframing</p>
-        <p>Research</p>
-      </div>
-
-      <div className="space-y-6">
-        <h3 className="font-medium">Resources</h3>
-        <p>Help Center</p>
-        <p>Blog</p>
-        <p>Tutorials</p>
-        <p>FAQs</p>
-      </div>
-
-      <div className="space-y-6">
-        <h3 className="font-medium">Support</h3>
-        <p>Contact Us</p>
-        <p>Developers</p>
-        <p>Documentation</p>
-        <p>Integrations</p>
-      </div>
-
-      <div className="space-y-6">
-        <h3 className="font-medium">Company</h3>
-        <p>About</p>
-        <p>Press</p>
-        <p>Events</p>
-        <div className="flex items-center gap-2">
-          <p>Request Demo</p>
-          <FaArrowRight />
-        </div>
-      </div>
-
-      <div className="border-t border-slate-700 space-y-6 pt-6">
+      <div className="space-y-6 pt-6 md:flex md:items-center md:space-y-0 md:justify-between xl:flex">
         <p>uteach @ 2023. All rights reserved.</p>
 
         <div className="flex gap-10 items-center">
